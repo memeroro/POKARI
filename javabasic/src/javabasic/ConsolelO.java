bdbd1f3f784cf112cbc0a70b237e33fa9d14174e
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-04-25 17:23:48 +0900

    fitst commit.

---
+++ javabasic/src/javabasic/ConsolelO.java
@@ -0,0 +1,14 @@
+package javabasic;
+
+import java.util.Scanner;
+
+public class ConsolelO {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int i = scanner.nextInt();
+		System.out.println(i);
+		
+		
+	} // main
+} //class
